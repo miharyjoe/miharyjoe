989396ab4adb3b3bab05edfda38e8e680b939d03
miharyjoel <miharyjoel@gmail.com>
2023-07-21 21:30:21 +0300

    feat: add script to inject quote in readme

--- readme.js
+++ readme.js
@@ -1,7 +1,6 @@
-
-<div align="center">
+module.export = `<div align="center">
 <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Fira+Code&duration=2500&pause=500&center=true&width=435&lines=Welcome+to+my+Github+%F0%9F%96%90;I'm+Mihary+Joel+ANDRIAMILANTO+;An+aspiring+software+developer" alt="Typing SVG" /></a>
-</div>
+ </div>
 
 <!--
 <div id="header" align="center">
@@ -17,16 +16,18 @@
 <div id="badge" align="center">
   <img src="https://komarev.com/ghpvc/?username=miharyjoe&style=flat-square&color=blue" alt=""/>
   <h1>
-    hey there
-    <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="30px"/>
-  </h1>
+  hey there
+  <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="30px"/>
+</h1>
 </div>
 
 <div id="quote" align="center">
-  <summary>Quote of the Day</summary>
-  <p>
-    “Ambition is the path to success. Persistence is the vehicle you arrive in”  –  Bill Bradley
-  </p>
+<summary>Quote of the Day</summary>
+<p>
+
+<#quote_of_the_day>
+
+</p>
 </div>
 
 <div align="center">
@@ -38,11 +39,11 @@
 
 I am a Developer <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30"> from Madagascar.
 
-- :telescope: I’m looking for a job in web development or mobile.
+- :telescope: I’m looking for a job in web developpement or mobile.
 
-- :zap: In my free time, I solve problems on codeWars, codingGame, and hackerrank. I also read tech articles.
+- :zap: In my free time, I solve problems on codeWars,codingGame and hakkerrank, i also read tech articles.
 
-- :mailbox: How to reach me: [![Linkedin Badge](https://img.shields.io/badge/-mihary-blue?style=flat&logo=Linkedin&logoColor=white)](https://www.linkedin.com/in/mihary-jo%C3%ABl-andriamilanto-374609233/L)
+- :mailbox:How to reach me: [![Linkedin Badge](https://img.shields.io/badge/-mihary-blue?style=flat&logo=Linkedin&logoColor=white)](https://www.linkedin.com/in/mihary-jo%C3%ABl-andriamilanto-374609233/L)
 
 ---
 
@@ -50,12 +51,13 @@ I am a Developer <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giph
 
 <div>
   <img src="https://github.com/devicons/devicon/blob/master/icons/java/java-original-wordmark.svg" title="Java" alt="Java" width="40" height="40"/>&nbsp;
-  <img src="https://github.com/devicons/devicon/blob/master/icons/react/react-original-wordmark.svg" title="React" alt="React" width="40" height="40"/>&nbsp;
-  <img src="https://github.com/devicons/devicon/blob/master/icons/linux/linux-plain.svg" title="Linux" alt="Linux" width="40" height="40"/>&nbsp;
-  <img src="https://github.com/devicons/devicon/blob/master/icons/postgresql/postgresql-plain.svg" title="Postgresql" alt="Postgresql" width="40" height="40"/>&nbsp;
-  <img src="https://github.com/devicons/devicon/blob/master/icons/typescript/typescript-original.svg" title="Typescript" alt="Typescript " width="40" height="40"/>&nbsp;
-  <img src="https://github.com/devicons/devicon/blob/master/icons/javascript/javascript-original.svg" title="JavaScript" alt="JavaScript" width="40" height="40"/>&nbsp;
-  <img src="https://github.com/devicons/devicon/blob/master/icons/nodejs/nodejs-original-wordmark.svg" title="NodeJS" alt="NodeJS" width="40" height="40"/>&nbsp;
+<img src="https://github.com/devicons/devicon/blob/master/icons/react/react-original-wordmark.svg" title="React" alt="React" width="40" height="40"/>&nbsp;
+<img src="https://github.com/devicons/devicon/blob/master/icons/linux/linux-plain.svg" title="Linux" alt="Linux" width="40" height="40"/>&nbsp;
+<img src="https://github.com/devicons/devicon/blob/master/icons/postgresql/postgresql-plain.svg" title="Postgresql" alt="Postgresql" width="40" height="40"/>&nbsp;
+<img src="https://github.com/devicons/devicon/blob/master/icons/typescript/typescript-original.svg" title="Typescript" alt="Typescript " width="40" height="40"/>&nbsp;
+<img src="https://github.com/devicons/devicon/blob/master/icons/javascript/javascript-original.svg" title="JavaScript" alt="JavaScript" width="40" height="40"/>&nbsp;
+<img src="https://github.com/devicons/devicon/blob/master/icons/nodejs/nodejs-original-wordmark.svg" title="NodeJS" alt="NodeJS" width="40" height="40"/>&nbsp;
+
 </div>
 
 ---
@@ -65,4 +67,4 @@ I am a Developer <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giph
 [![GitHub Streak](http://github-readme-streak-stats.herokuapp.com?user=miharyjoe&theme=blueberry&hide_border=true&date_format=M%20j%5B%2C%20Y%5D)](https://git.io/streak-stats)
 
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=miharyjoe&layout=compact&theme=vision-friendly-dark)](https://github.com/anuraghazra/github-readme-stats)
-
+`;
